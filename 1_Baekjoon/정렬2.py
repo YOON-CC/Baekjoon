a4c89f2b5b759f0b79be3f5fb095556442df5b01
YOON-CC <whdbscks77@gmail.com>
2022-01-23 18:14:24 +0900

    메뫼리 아끼기

---
+++ 1_Baekjoon/정렬2.py
@@ -0,0 +1,16 @@
+# 10989번
+#매모리를 효율적으로 사용하는 방법
+
+import sys
+
+n = int(sys.stdin.readline())
+num_list = [0] * 10001 # 이러면 배열의 길이가 10001이 된다.
+
+for _ in range(n):
+    num_list[int(sys.stdin.readline())] += 1 # 입력한 인덱스에 1이 들어간다.(+=)로 중복처리
+
+for i in range(10001):
+    if num_list[i] != 0:
+        for j in range(num_list[i]): # 결국 든것은 1이니 한번 반복이다. 중복은 그만큼 수 
+            print(i)
+
