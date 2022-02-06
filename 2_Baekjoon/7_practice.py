58330b2a6f9a8549da9fdb09dcd30827c351aa99
YOON-CC <whdbscks77@gmail.com>
2022-02-06 18:18:44 +0900

    알고리즘 스터디(DP_1)

---
+++ 2_Baekjoon/7_practice.py
@@ -0,0 +1,10 @@
+n=int(input())
+a=[list(map(int,input().split())) for _ in range(n)]
+
+for i in range(1,n):
+    a[i][0]+=min(a[i-1][1],a[i-1][2]) #빨강
+    a[i][1]+=min(a[i-1][0],a[i-1][2]) #초록
+    a[i][2]+=min(a[i-1][0],a[i-1][1]) #파랑
+
+print(a)
+print(min(a[n-1]))
