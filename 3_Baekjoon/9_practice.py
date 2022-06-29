eac615d52470eb2cfa7fac171aac670022ddf13d
YOON-CC <whdbscks77@gmail.com>
2022-06-30 04:02:14 +0900

    끝자리0인 문제

---
+++ 3_Baekjoon/9_practice.py
@@ -0,0 +1,6 @@
+#끝자리가 0이라는 문제는
+#2와 5로 조작을 할 수 있다.
+
+#참고
+#https://www.youtube.com/watch?v=nNaMOy79J1s
+#https://somjang.tistory.com/entry/BaeKJoon-2004%EB%B2%88-%EC%A1%B0%ED%95%A9-0%EC%9D%98-%EA%B0%9C%EC%88%98-Python
