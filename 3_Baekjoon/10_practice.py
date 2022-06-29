5766ee3b5245f2d038690a24822ebd876b16bb11
YOON-CC <whdbscks77@gmail.com>
2022-06-30 04:17:52 +0900

    최대공약수(GCD), 최소공배수(LCM) 문제

---
+++ 3_Baekjoon/10_practice.py
@@ -0,0 +1,9 @@
+#최대공약수(GCD) - 라이브러리 사용
+#최소공배수(LCM) => LCM(a, b) = a x b / GCD(a, b) <- 진짜 이것만 알면 됨
+
+#참고사이트
+#http://www.tcpschool.com/codingmath/common
+from math import gcd
+
+
+print(gcd(3,7,12))
