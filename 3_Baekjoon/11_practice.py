a97cee491898e8c13cf390e83d12fc3c9227c859
YOON-CC <whdbscks77@gmail.com>
2022-07-01 22:18:09 +0900

    행복하자

---
+++ 3_Baekjoon/11_practice.py
@@ -0,0 +1,33 @@
+#(1) 전체 수 만큼 True의 리스트를 생성해준다.
+#(2) 2부터 +1씩 해주면서 그 배수에 해당하는 값들을 False로 바꿔준다.
+r= 1000000
+
+check = [True for _ in range(r)]
+
+for i in range(2,int(r**0.6)):
+    if check[i]==True:
+        for j in range(i*2, r, i): # 마지막 i 즉, 튜플은 그 다음이 i만큼 증가해서 나타낸다는 것이다.
+            if check[j] == True :
+                check[j] = False  
+                              
+import sys
+input = sys.stdin.readline
+
+how_many = int(input()) 
+
+for _ in range(how_many):                 
+    n = int(input()) 
+    if n == 4:
+        print(1)
+        continue
+    cnt = 0
+    for i in range(3,r):
+        if check[i] == True:
+            if check[n-i] == True : #여러 가지라면, b-a가 가장 큰 것을 출력을 해야하기 때문에 이렇게 한것이다.
+                if(i>(n-i)):
+                    break
+                else:
+                    cnt+=1
+    print(cnt)
+    
+    ##행복하자
